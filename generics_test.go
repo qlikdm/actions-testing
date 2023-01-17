c79c453c9f490865973d83e2f36e5dd159d6c64c
Max Fogwall <kdm@qlik.com>
2023-01-17 13:58:14 +0100

    Move tests around a little.

--- generics_test.go
+++ generics_test.go
@@ -6,6 +6,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNoDuplicateValues(t *testing.T) {
+	actual := GetSliceWithoutDuplicates([]string{"one", "two", "three"})
+	expected := []string{"one", "two", "three"}
+	assert.Equal(t, actual, expected)
+}
+
+func TestSkipDuplicateValues(t *testing.T) {
+	actual := GetSliceWithoutDuplicates([]string{"one", "two", "one", "one", "three"})
+	expected := []string{"one", "two", "three"}
+	assert.Equal(t, actual, expected)
+}
+
 func TestNoZeroValues(t *testing.T) {
 	actual := GetSliceWithoutZeroValues([]int{1, 2, 2, -1, 4})
 	expected := []int{1, 2, 2, -1, 4}
@@ -21,17 +33,5 @@ func TestSkipZeroValues(t *testing.T) {
 func TestSkipZeroValuedStrings(t *testing.T) {
 	actual := GetSliceWithoutZeroValues([]string{"one", "", "two", "two", "", "three"})
 	expected := []string{"one", "two", "two", "three"}
-	assert.Equal(t, actual, expected)
-}
-
-func TestNoDuplicateValues(t *testing.T) {
-	actual := GetSliceWithoutDuplicates([]string{"one", "two", "three"})
-	expected := []string{"one", "two", "three"}
-	assert.Equal(t, actual, expected)
-}
-
-func TestSkipDuplicateValues(t *testing.T) {
-	actual := GetSliceWithoutDuplicates([]string{"one", "two", "one", "one", "three"})
-	expected := []string{"one", "two", "three"}
 	assert.Equal(t, actual, expected)
 }
